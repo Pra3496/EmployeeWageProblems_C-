6064d614abda7741c15d7e6237ee0888d7e6fb2a
Pranav Waghmare <piyushw55@gmail.com>
2022-12-19 15:17:39 +0530

    Calculating Employee Wage with Part and Full Time using Switch Cases

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -2,12 +2,11 @@
 {
     internal class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            //Constants
-            const int IS_PART_TIME = 1;
-            const int IS_FULL_TIME = 2;
-            const int EMP_RATE_PER_HOUR = 20;
 
             //variables
             int empHrs = 0;
@@ -19,19 +18,19 @@
 
             int empCheck = random.Next(0, 3); ;
 
-            if (empCheck == IS_PART_TIME)
+            switch(empCheck)
             {
-                empHrs = 4;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0; 
+                    break;
             }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
-            }
-
+            
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("emp Wage : " + empWage);
         }
