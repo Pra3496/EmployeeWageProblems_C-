1bda9e069a59ffdc0ce6243995a2fc973f6b8827
Pranav Waghmare <piyushw55@gmail.com>
2022-12-19 14:44:58 +0530

    Calculating Employee Wage

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -6,6 +6,11 @@
         {
             //Constants
             const int IS_FULL_TIME = 1;
+            const int EMP_RATE_PER_HOUR = 20;
+
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
 
             Random random = new Random();
 
@@ -15,12 +20,15 @@
 
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("emp Wage : " + empWage);
         }
     }
 }
